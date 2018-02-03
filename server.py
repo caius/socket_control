41e32c96c81d1c6cf2dd81d62de7c63ec8aae09a
Caius Durling <dev@caius.name>
2018-02-03 19:56:43 +0000

    Server?

--- server.py
+++ server.py
@@ -1,31 +1,48 @@
-from bottle import route, run, template
+from bottle import Bottle
+from rpi_rf import RFDevice
+
+# We expect to use GPIO pin 17 (GPIO.0) for tx
+rfdevice = RFDevice(17)
+rfdevice.enable_tx()
+
+# Webserver
+app = Bottle()
+
+# Sniffed these off the remote
+protocol = 1
+pulseLength = 314
 
 on_codes = {
-  'A': '10',
-  'B': '20',
-  'C': '30',
-  'D': '40',
+  'A': '1394001',
+  'B': '1397073',
+  'C': '1397841',
+  'D': '1398033',
 }
 
 off_codes = {
-  'A': '15',
-  'B': '25',
-  'C': '35',
-  'D': '45',
+  'A': '1394004',
+  'B': '1397076',
+  'C': '1397844',
+  'D': '1398036',
 }
 
+# Send a code for a group
 def codesend(code):
-  print("Sending code %s" % code)
+  print("Sending code=%s, protocol=%s, pulseLength=%s" % (code, protocol, pulseLength))
+  rfdevice.tx_code(code, protocol, pulselength)
   return
 
-@route('/switches/<id>/on', method='POST')
+@app.post('/switches/<id:re:[A-D]>/on', method='POST')
 def switch_on(id):
+  print("switch_on id='%s'" % (id))
   codesend(on_codes[id])
-  return '{"result":"ok"}\n'
+  return dict({'result': 'ok'})
 
-@route('/switches/<id>/off', method='POST')
+@app.post('/switches/<id:re:[A-D]>/off')
 def switch_off(id):
+  print("switch_off id='%s'" % (id))
   codesend(off_codes[id])
-  return '{"result":"ok"}\n'
+  return dict({'result': 'ok'})
 
-run()
+# Kick it off
+app.run(host='0.0.0.0', port=8080)
